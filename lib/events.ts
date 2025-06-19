80a4c26c90a15152058b22ac935d309df5929704
Andre601 <github@andre601.ch>
2025-06-19 14:03:43 +0200

    Create own embeds

--- lib/events.ts
+++ lib/events.ts
@@ -1,27 +1,16 @@
 export const EventFlags = {
+  // Pushes
   PUSH: 1 << 0,
+  // Releases
   RELEASE_PUBLISH: 1 << 1,
   RELEASE_EDIT: 1 << 2,
   RELEASE_DELETE: 1 << 3,
+  // Issues
   ISSUE_CREATE: 1 << 4,
   ISSUE_EDIT: 1 << 5,
   ISSUE_CLOSE: 1 << 6,
-  PR_CREATE: 1 << 7,
-};
-
-export const EventLabels: Record<keyof typeof EventFlags, string> = {
-  // Pushes
-  PUSH: 'Push Events',
-  // Releases
-  RELEASE_PUBLISH: 'Release Published',
-  RELEASE_EDIT: 'Release Edited',
-  RELEASE_DELETE: 'Release Deleted',
-  // Issues
-  ISSUE_CREATE: 'Issue Created',
-  ISSUE_EDIT: 'Issue Edited',
-  ISSUE_CLOSE: 'Issue Closed',
   // Pull requests
-  PR_CREATE: 'Pull Request Created',
+  PR_CREATE: 1 << 7,
 };
 
 export type EventKey = keyof typeof EventFlags;
--- pages/api/webhooks/[id]/[token]/[code].ts
+++ pages/api/webhooks/[id]/[token]/[code].ts
@@ -15,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const rawBody = req.body;
   const header = req.headers['x-gitea-event'] as string;
-  const decodedFlags = parseInt(code, 36); // decode base36
+  const decodedFlags = parseInt(code, 36);
 
   const event = determineExactEvent(header, rawBody);
   if (!event || (EventFlags[event] & decodedFlags) !== EventFlags[event]) {
@@ -33,6 +33,102 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   res.status(204).end();
 };
+
+const payloadCreator = (event: string, body: any) => {
+  switch (event) {
+    case 'PUSH':
+      return {
+        embeds: {
+          title: `[${body.repository?.full_name}:${body.ref}] ${body.commits?.size()} new commits`,
+          color: 1754624,
+        }
+      }
+    case 'RELEASE_PUBLISH':
+      return {
+        embeds: {
+          title: `[${body.repository?.full_name}] Release created: ${body.release?.name}`,
+          description: `${body.release?.body}`,
+          url: `${body.release?.html_url}`,
+          color: 1754624,
+          author: {
+            name: `${body.release?.author?.login}`,
+            url: `${body.release?.author?.html_url}`,
+            icon_url: `${body.release?.author?.avatar_url}`
+          }
+        }
+      }
+    case 'RELEASE_EDIT':
+      return {
+        embeds: {
+          title: `[${body.repository?.full_name}] Release updated: ${body.release?.name}`,
+          description: `${body.release?.body}`,
+          url: `${body.release?.html_url}`,
+          color: 16767280,
+          author: {
+            name: `${body.release?.author?.login}`,
+            url: `${body.release?.author?.html_url}`,
+            icon_url: `${body.release?.author?.avatar_url}`
+          }
+        }
+      }
+    case 'RELEASE_DELETE':
+      return {
+        embeds: {
+          title: `[${body.repository?.full_name}] Release deleted: ${body.release?.name}`,
+          description: `${body.release?.body}`,
+          url: `${body.release?.html_url}`,
+          color: '16724530',
+          author: {
+            name: `${body.release?.author?.login}`,
+            url: `${body.release?.author?.html_url}`,
+            icon_url: `${body.release?.author?.avatar_url}`
+          }
+        }
+      }
+    case 'ISSUE_CREATE':
+      return {
+        embeds: {
+          title: `[${body.repository?.full_name}] Issue opened: #${body.number} ${body.issue?.title}`,
+          description: `${body.issue?.body}`,
+          url: `${body.issue?.html_url}`,
+          color: 15426592,
+          author: {
+            name: `${body.issue?.user?.login}`,
+            url: `${body.issue?.user?.html_url}`,
+            icon_url: `${body.issue?.user?.avatar_url}`
+          }
+        }
+      }
+    case 'ISSUE_EDIT':
+      return {
+        embeds: {
+          title: `[${body.repository?.full_name}] Issue edited: #${body.number} ${body.issue?.title}`,
+          description: `${body.issue?.body}`,
+          url: `${body.issue?.html_url}`,
+          color: 16767280,
+          author: {
+            name: `${body.issue?.user?.login}`,
+            url: `${body.issue?.user?.html_url}`,
+            icon_url: `${body.issue?.user?.avatar_url}`
+          }
+        }
+      }
+    case 'ISSUE_EDIT':
+      return {
+        embeds: {
+          title: `[${body.repository?.full_name}] Issue closed: #${body.number} ${body.issue?.title}`,
+          description: `${body.issue?.body}`,
+          url: `${body.issue?.html_url}`,
+          color: 16724530,
+          author: {
+            name: `${body.issue?.user?.login}`,
+            url: `${body.issue?.user?.html_url}`,
+            icon_url: `${body.issue?.user?.avatar_url}`
+          }
+        }
+      }
+  }
+}
 
 export const config = {
   api: {
